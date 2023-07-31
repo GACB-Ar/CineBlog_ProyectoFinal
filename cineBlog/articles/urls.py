68fcb362a95bf7fbbd70e9853b142e0b41a82410
GACB-Ar <gerocb.lab@gmail.com>
2023-07-31 13:53:39 -0300

    added functionalities to article.html and article_detail.html

--- cineBlog/articles/urls.py
+++ cineBlog/articles/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from . import views
-from .views import article_view, create_article
+from .views import article_view, create_article, article_detail
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('article/', article_view, name='article'),
-    path('create_article/', create_article, name='create')
+    path('articles/', article_view, name='articles'),
+    path('create_article/', create_article, name='create'),
+    path('article_detail/<int:id>', article_detail,  name='article_detail')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- cineBlog/articles/views.py
+++ cineBlog/articles/views.py
@@ -1,11 +1,26 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from .models import Article
 from .forms import Create_new_article
 from django.contrib import messages
 
 # Create your views here.
 def article_view(request):
-    return render(request, 'article_layouts/article.html')
+    articles = Article.objects.all()
+
+    context = {
+        'articles': articles,
+    }
+    return render(request, 'article_layouts/article.html', context)
+
+def article_detail(request, id):
+    article = get_object_or_404(Article, article_id=id)
+   
+    context = {
+        'article': article,
+    }
+
+    return render(request, 'article_layouts/article_detail.html', context)
 
 @login_required
 def create_article(request):
@@ -21,4 +36,7 @@ def create_article(request):
     else:
         form = Create_new_article()
     
-    return render(request, 'article_layouts/create_article.html', {'form': form})
+    return render(request, 'article_layouts/create_article.html', {'form':form})
+
+
+
