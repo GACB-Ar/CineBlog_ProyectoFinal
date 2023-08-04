69b08f28301213e070b782c49b9af324780e528b
GACB-Ar <gerocb.lab@gmail.com>
2023-08-04 16:13:12 -0300

    fixed media folder

--- cineBlog/cineBlog/settings.py
+++ cineBlog/cineBlog/settings.py
@@ -129,8 +129,8 @@ STATICFILES_DIRS = (os.path.join(os.path.dirname(BASE_DIR),'cineBlog/static'),)
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-MEDIA_URL = 'media/'
-MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR),'media')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR),'cineBlog/media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
